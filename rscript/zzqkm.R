4c7ac58a47ed8f6ce582eb5889f961b751b4d54a
ericluo <eric@Eric.local>
2013-12-17 23:39:16 +0800

    runnable demos

--- rscript/zzqkm.R
+++ rscript/zzqkm.R
@@ -1,16 +1,31 @@
-zzqmk = read.csv("~/workspace/dast/data/zzqkm_T_RMB_L3.csv", header = F, encoding='utf-8')
-zzqmk$V10 = as.Date(as.character(zzqmk$V10), "%Y%m%d")
+cnames <- c("科目编码", "", "", "科目名称", "科目层级", "科目类别", 
+              "借贷","借方金额", "贷方金额", "采集日期")
+zzqkm <- read.csv("~/workspace/dast/data/zzqkm_T_RMB_L3.csv", header = F, 
+                 col.names = cnames, encoding='utf-8')
+zzqkm <- transform(zzqkm, 科目编码 = as.factor(科目编码),
+                   科目名称 = as.factor(科目名称),
+                   科目类别 = as.factor(科目类别),
+                   借贷 = as.factor(借贷),
+                   借方金额 = 借方金额 / 100000000,
+                   贷方金额 = 贷方金额 / 100000000,
+                   采集日期 = as.Date(as.character(采集日期), "%Y%m%d"))
+
+l1 <- zzqkm[which(zzqkm$科目层级 == 1),]
+l1.debit <- l1[l1$科目编码 %in% c("2001", "2010", "2015"),]
+l1.debit <- l1.debit[order(l1.debit$采集日期),]
 
 library(ggplot2)
 library(scales)
 library(ggthemes)
 
-km <- zzqmk[which(zzqmk$V1 == '2001'), ]
-km <- km[order(km$V10),]
-km <- transform(km,V9 = (V9 / 10000))
-pt <- qplot(V10, V9, data = km, geom='path', xlab='日期', ylab='金额')
+start <- min(l1.debit$采集日期)
+end <- max(l1.debit$采集日期)
+pt <- qplot(采集日期, 贷方金额, data = l1.debit, geom='path', xlab='日期', ylab='金额')
 # pt <- pt + theme_economist() + theme(text = element_text(family='STKaiti')) 
-pt <- pt + theme(text = element_text(family='STKaiti')) 
+pt <- pt + geom_smooth()
+pt <- pt + theme(text = element_text(family='STKaiti'))
+pt <- pt + scale_x_date(limits = c(start, end), labels = date_format('%m/%y'), breaks = "months")
 # pt + theme(aspect.ratio = 1 / 2) + scale_x_date()
 # pt + scale_x_date(labels = date_format('%m/%y'),breaks = "3 months") + scale_y_log10()
-pt + scale_x_date(breaks = "3 months")
+pt <- pt + facet_grid(科目名称 ~ ., scale="free_y") 
+pt
