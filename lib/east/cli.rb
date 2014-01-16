49dd49450df7c46f458f9914f24e7758f7a1b1d6
eric <eric@nagios.hub.cbrc>
2014-01-16 13:50:32 +0800

    separate database setup from schema

--- lib/east/cli.rb
+++ lib/east/cli.rb
@@ -29,23 +29,34 @@ module East
       end
     end
 
-    desc "setup", "setup database for given schemas"
-    option :schemas, type: :string, default: :all
-    def setup
+    desc "create DATABASE DIR", "create database on the given path"
+    def create(dbname, dir)
+      # create database 
+      run "db2 create database #{dbname} automatic storage yes on #{dir} using codeset gbk territory cn"
+      # add user for database
+      run "sudo useradd -g db2iadm1 #{dbname}"
       # create database eastst
       run "db2 -tvf sql/create_eastst.sql > log/create_eastst.log"
+    end
 
+    desc "setup", "setup database and dbuser for given schemas"
+    option :schemas, type: :string, default: :all
+    def setup
       schemas(options).each do |schema|
         schema = schema.downcase
+
         # create table for schema
         run "db2 -tvf sql/#{schema}/create_table.sql > log/create_table_#{schema}.log"
         # grant rights to user
         run "db2 -tvf sql/#{schema}/grant.sql > log/grant_#{schema}.log"
+        # create user for schema
+        # run "sudo useradd #{schema}"
+        # run "sudo passwd #{schema}"
       end
     end
 
-    option :glob, aliases: ['-g'], type: :string, default: '*.txt'
     desc "check DIR", "check whether the name of files in the given DIR is valid"
+    option :glob, aliases: ['-g'], type: :string, default: '*.txt'
     def check(dir)
       DataLoader.new(dir, options[:glob]).check
     end
